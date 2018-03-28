1eb6152ea0df98257dab929273c48b37a47b9e6d
Jonathan Plattner <jsp263@cornell.edu>
2018-03-28 17:34:43 -0400

    New color scale.

--- DE1_SoC_Computer.v
+++ DE1_SoC_Computer.v
@@ -443,8 +443,24 @@ multi_solver #(NUM_SOLVERS) solver (
     .done(solver_done)
 );
 
-assign stream_data[3:0] = solver_data_out;
-assign stream_data[7:4] = solver_data_out;
+assign stream_data = 
+		solver_data_out == 4'b0000 ? 8'b00000000 :
+		solver_data_out == 4'b0001 ? 8'b00000100 :
+		solver_data_out == 4'b0010 ? 8'b00001000 :
+		solver_data_out == 4'b0011 ? 8'b00001100 :
+		solver_data_out == 4'b0100 ? 8'b00010000 :
+		solver_data_out == 4'b0101 ? 8'b00010100 :
+		solver_data_out == 4'b0110 ? 8'b00011000 :
+		solver_data_out == 4'b0111 ? 8'b00011101 :
+		solver_data_out == 4'b1000 ? 8'b00011110 :
+		solver_data_out == 4'b1001 ? 8'b00011011 :
+		solver_data_out == 4'b1010 ? 8'b00010111 :
+		solver_data_out == 4'b1011 ? 8'b00010011 :
+		solver_data_out == 4'b1100 ? 8'b00001111 :
+		solver_data_out == 4'b1101 ? 8'b00001011 :
+		solver_data_out == 4'b1110 ? 8'b00000111 :
+		solver_data_out == 4'b1111 ? 8'b00000011 :
+									 8'b11100000;
 
 assign hex5_hex0 = solve_time[23:0];
 assign LEDR[7:0] = solve_time[31:24];
