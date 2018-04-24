a48977315854e3ee77bfda3c1aba62eb4e2e4eec
Max Rademacher <maxrademacher@yahoo.com>
2018-04-24 16:46:50 -0400

    Comment out print statements in bignum.cpp and added bignum testing harness

---
+++ hps_solver/test_bignum.cpp
@@ -0,0 +1,92 @@
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+#include "bignum.h"
+
+#define EPSILON 0
+
+double get_rand() {
+    int range = 22;
+    return (((double) rand()) / RAND_MAX) * range - (range / 2);
+}
+
+void assertEquals(double a, BigNum num) {
+    double num_val = num.toDouble();
+    if (std::abs(a - num_val) > EPSILON) {
+        std::cout << a << " != " << num.toString() << " (" << num_val << ")" << "\n";
+        std::cout << "Failed\n";
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+
+    int num_tests = 10000;
+
+    std::cout << "Testing double conversion\n";
+    for (int i = 0; i < num_tests; i++) {
+        double val = get_rand();
+        BigNum num(val);
+        assertEquals(val, num);
+        std::cout << "\r" << (i + 1) << "     " << std::flush;
+    }
+    std::cout << "Passed\n\n";
+
+    std::cout << "Testing addition\n";
+    for (int i = 0; i < num_tests; i++) {
+        double a = get_rand();
+        double b = get_rand();
+        BigNum big_a(a);
+        BigNum big_b(b);
+        BigNum big_sum = a + b;
+        assertEquals(a + b, big_sum);
+        std::cout << "\r" << (i + 1) << "     " << std::flush;
+    }
+    std::cout << "Passed\n\n";
+
+    std::cout << "Testing negation\n";
+    for (int i = 0; i < num_tests; i++) {
+        double val = get_rand();
+        BigNum num(val);
+        assertEquals(-val, -num);
+        std::cout << "\r" << (i + 1) << "     " << std::flush;
+    }
+    std::cout << "Passed\n\n";
+
+    std::cout << "Testing subtraction\n";
+    for (int i = 0; i < num_tests; i++) {
+        double a = get_rand();
+        double b = get_rand();
+        BigNum big_a(a);
+        BigNum big_b(b);
+        BigNum big_diff = a - b;
+        assertEquals(a - b, big_diff);
+        std::cout << "\r" << (i + 1) << "     " << std::flush;
+    }
+    std::cout << "Passed\n\n";
+
+    std::cout << "Testing multiplication\n";
+    for (int i = 0; i < num_tests; i++) {
+        double a = get_rand();
+        double b = get_rand();
+        BigNum big_a(a);
+        BigNum big_b(b);
+        BigNum big_product = a * b;
+        assertEquals(a * b, big_product);
+        std::cout << "\r" << (i + 1) << "     " << std::flush;
+    }
+    std::cout << "Passed\n\n";
+
+/*
+    BigNum foo(1.5);
+    BigNum bar(0.5);
+    std::cout << "foo: " << foo.toString() << "\n";
+    std::cout << "bar: " << bar.toString() << "\n";
+    BigNum bla(foo * bar);
+    std::cout << "end\n";
+    std::cout << get_rand() << "\n";*/
+    return 0;
+}
