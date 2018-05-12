63366618e2c3030b64680c897c51d699141534ee
Michael Henning <drawoc@darkrefraction.com>
2018-05-12 16:37:15 -0400

    Make tile_solver compute a full tile.

--- tile_solver_legit.sv
+++ tile_solver_legit.sv
@@ -4,6 +4,7 @@
 `include "solver.v"
 
 module tile_solver_legit #(
+    parameter TILE_WIDTH_BITS   = 5,
     parameter LIMB_INDEX_BITS   = 6,
     parameter LIMB_SIZE_BITS    = 27,
     parameter DIVERGENCE_RADIUS = 4
@@ -100,24 +101,50 @@ solver #(
 );
 
 // Read/write logic
-`define ST_READ_INPUT       2'd0
-`define ST_WAIT_CYCLE       2'd1
-`define ST_SOLVING_PIXEL    2'd2
-`define ST_WRITE_OUTPUT     2'd3
+`define ST_READ_INPUT       3'd0
+`define ST_WAIT_CYCLE       3'd1
+`define ST_SOLVING_PIXEL    3'd2
+`define ST_WRITE_OUTPUT     3'd3
+`define ST_SET_LIMB_1       3'd4
+`define ST_SET_LIMB_2       3'd5
 
-reg [1:0] state;
-reg [1:0] next_state;
+reg [2:0] state;
+reg [2:0] next_state;
 
 reg addr;
 reg next_addr;
+
+reg [31:0] iterator_shift;
+reg [31:0] next_iterator_shift;
+
+reg [TILE_WIDTH_BITS * 2 - 1 : 0] iterator;
+reg [TILE_WIDTH_BITS * 2 - 1 : 0] next_iterator;
+
+reg [LIMB_INDEX_BITS-1 : 0] num_limbs;
+reg [LIMB_INDEX_BITS-1 : 0] next_num_limbs;
+
+reg [LIMB_SIZE_BITS * 2 - 1 : 0] lowest_limbs_re;
+reg [LIMB_SIZE_BITS * 2 - 1 : 0] next_lowest_limbs_re;
+reg [LIMB_SIZE_BITS * 2 - 1 : 0] lowest_limbs_im;
+reg [LIMB_SIZE_BITS * 2 - 1 : 0] next_lowest_limbs_im;
 
 always @(posedge clock) begin
     if (reset) begin
         state <= `ST_READ_INPUT;
         addr <= 0;
+        iterator_shift <= 0;
+        iterator <= 0;
+        num_limbs <= 0;
+        lowest_limbs_re <= 0;
+        lowest_limbs_im <= 0;
     end else begin
         state <= next_state;
         addr <= next_addr;
+        iterator_shift <= next_iterator_shift;
+        iterator <= next_iterator;
+        num_limbs <= next_num_limbs;
+        lowest_limbs_re <= next_lowest_limbs_re;
+        lowest_limbs_im <= next_lowest_limbs_im;
     end
 end
 
@@ -156,11 +183,28 @@ always @(posedge clock) begin
     end
 end
 
+// ST_SET_LIMB
+wire [TILE_WIDTH_BITS-1 : 0] x_for_pixel;
+wire [TILE_WIDTH_BITS-1 : 0] y_for_pixel;
+assign x_for_pixel = iterator[TILE_WIDTH_BITS - 1 : 0];
+assign y_for_pixel = iterator[TILE_WIDTH_BITS * 2 - 1 : TILE_WIDTH_BITS];
+
+wire [LIMB_SIZE_BITS * 2 - 1 : 0] lowest_limbs_re_for_pixel;
+wire [LIMB_SIZE_BITS * 2 - 1 : 0] lowest_limbs_im_for_pixel;
+assign lowest_limbs_re_for_pixel = lowest_limbs_re | ({ /*(LIMB_SIZE_BITS * 2)*/{1'b0}, x_for_pixel} << iterator_shift);
+assign lowest_limbs_im_for_pixel = lowest_limbs_im | ({ /*(LIMB_SIZE_BITS * 2)*/{4{1'b0}}, y_for_pixel} << iterator_shift);
+
 // The logic
 always @(*) begin
     // Set defaults
     next_state = state;
     next_addr = addr;
+    next_iterator_shift = iterator_shift;
+    next_iterator = iterator;
+    next_num_limbs = num_limbs;
+
+    next_lowest_limbs_re = lowest_limbs_re;
+    next_lowest_limbs_im = lowest_limbs_im;
 
     in_ready = 0;
 
@@ -196,6 +240,8 @@ always @(*) begin
     if (state == `ST_READ_INPUT) begin
         in_ready = 1;
 
+        next_iterator = 0;
+
         next_re_write_index = re_write_index;
         next_im_write_index = im_write_index;
 
@@ -208,23 +254,39 @@ always @(*) begin
             else if (in_data_type == 3'd1) begin
                 next_solver_wr_num_limbs_en = 1;
                 next_solver_num_limbs_data = in_data_decoded;
+                next_num_limbs = in_data_decoded;
             end
             // Write a limb of c_real.
             else if (in_data_type == 3'd2) begin
                 next_re_write_index = re_write_index + 1;
                 next_solver_wr_index = re_write_index;
                 next_solver_wr_real_en = 1;
+
+                next_lowest_limbs_re = {
+                    lowest_limbs_re[LIMB_SIZE_BITS - 1 : 0],
+                    in_data_decoded[LIMB_SIZE_BITS - 1 : 0]
+                };
             end
             // Write a limb of c_imag.
             else if (in_data_type == 3'd3) begin
                 next_im_write_index = im_write_index + 1;
                 next_solver_wr_index = im_write_index;
                 next_solver_wr_imag_en = 1;
+
+                next_lowest_limbs_im = {
+                    lowest_limbs_im[LIMB_SIZE_BITS - 1 : 0],
+                    in_data_decoded[LIMB_SIZE_BITS - 1 : 0]
+                };
             end
             // Write the maximum number of iterations
             else if (in_data_type == 3'd4) begin
                 next_solver_wr_iter_lim_en = 1;
                 next_solver_iter_lim_data = in_data_decoded;
+            end
+            // Write the number of bits, from the right hand edge of the last
+            // limb, to offset the x/y value of the iteration from.
+            else if (in_data_type == 3'd5) begin
+                next_iterator_shift = in_data_decoded;
             end
 
 
@@ -254,16 +316,55 @@ always @(*) begin
 
     // ST_WRITE_OUTPUT
     if (state == `ST_WRITE_OUTPUT) begin
-        $display("iterations: %d", saved_iterations);
+        //$display("iterations: %d", saved_iterations);
 
-        out_addr = addr;
+        out_addr = addr + 2 * iterator;
         out_data = saved_iterations;
         out_valid = 1;
 
         if (out_ready) begin
-            next_state = `ST_READ_INPUT;
+            if (iterator == '1) begin
+                // We've iterated through all the pixels
+                next_state = `ST_READ_INPUT;
+            end else begin
+                // Calculate the next pixel
+                next_iterator = iterator + 1;
+                next_state = `ST_SET_LIMB_1;
+            end
         end
     end
+
+    // ST_SET_LIMB_1
+    if (state == `ST_SET_LIMB_1) begin
+        next_solver_real_data = lowest_limbs_re_for_pixel[LIMB_SIZE_BITS * 2 - 1 : LIMB_SIZE_BITS];
+        next_solver_imag_data = lowest_limbs_im_for_pixel[LIMB_SIZE_BITS * 2 - 1 : LIMB_SIZE_BITS];
+
+        next_solver_wr_real_en = 1;
+        next_solver_wr_imag_en = 1;
+
+        next_solver_wr_index = num_limbs - 2;
+
+        next_state = `ST_SET_LIMB_2;
+    end
+
+    // ST_SET_LIMB_2
+    if (state == `ST_SET_LIMB_2) begin
+        next_solver_real_data = lowest_limbs_re_for_pixel[LIMB_SIZE_BITS - 1 : 0];
+        next_solver_imag_data = lowest_limbs_im_for_pixel[LIMB_SIZE_BITS - 1 : 0];
+
+        next_solver_wr_real_en = 1;
+        next_solver_wr_imag_en = 1;
+
+        next_solver_wr_index = num_limbs - 1;
+
+        next_solver_start = 1;
+        next_state = `ST_WAIT_CYCLE; //`ST_START_SOLVER;
+    end
+
+    // ST_START_SOLVER
+    /*if (state == `ST_START_SOLVER) begin
+        next_state = `ST_WAIT_CYCLE;
+    end*/
 end
 
 endmodule
